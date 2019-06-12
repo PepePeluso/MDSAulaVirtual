2b1749338f631206937a732684a993c307cc23c2
Diego Castillo <Diego Castillo@172.16.32.221>
2019-06-12 13:25:47 -0500

    Merge origin/master
    Conflicts:
    	ProyectoConcursosMDS/archivo.conc

---
+++ ReservasAulaVirtual/src/Clases/Administrador.java
@@ -0,0 +1,14 @@
+/***********************************************************************
+ * Module:  Administrador.java
+ * Author:  Diego Castillo
+ * Purpose: Defines the Class Administrador
+ ***********************************************************************/
+
+import java.util.*;
+
+/** @pdOid 5af5ff75-e825-4a03-ba72-ccd794218e7b */
+public class Administrador {
+   /** @pdOid ee40a540-ea60-4e87-83f2-426978780b5c */
+   private Login longiAdministrador;
+
+}
