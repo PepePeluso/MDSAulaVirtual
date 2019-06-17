afbce93b67df8c30a2d558a248276ead106dc51d
Diego Castillo <Diego Castillo@172.16.32.221>
2019-06-17 14:49:18 -0500

    Merge origin/master
    Conflicts:
    	ProyectoConcursosMDS/archivo.conc

--- ReservasAulaVirtual/src/Clases/Main.java
+++ ReservasAulaVirtual/src/Clases/Main.java
@@ -1,9 +1,12 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Clases;
+
 
 
 
@@ -13,8 +16,10 @@ package Clases;
  * @author Pepe Peluso
  */
 public class Main {
-    public static void main(String[] LoQueTocaHacerPorlosEfectos){
-        
+    public static void main(String[] args) {
+        AulaVirtual aulaVirtual=new AulaVirtual();
+        aulaVirtual.agregarReserva(new Reserva("Lunes", 7, 8, "Fausto", "1004291447", "Tomar un tragito"));
+        JOptionPane.showMessageDialog(null, "Hola");
     }
     
 }
