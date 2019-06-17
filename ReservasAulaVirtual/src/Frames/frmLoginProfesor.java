b47a82eaf5bd1e2d66abed28c09b0bd2fadc0dbb
Pepe Peluso <Pepe Peluso@DESKTOP-D80TVP9>
2019-06-17 14:19:47 -0500

    Remove unused imports

--- ReservasAulaVirtual/src/Frames/frmLoginProfesor.java
+++ ReservasAulaVirtual/src/Frames/frmLoginProfesor.java
@@ -108,13 +108,17 @@ public class frmLoginProfesor extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         jLabel1.setText("Contraseña");
         rSPanelGradiente1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 350, 400, -1));
-        rSPanelGradiente1.add(txtUsuarioProfesor, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 300, 400, -1));
+
+        txtUsuarioProfesor.setBorder(null);
+        rSPanelGradiente1.add(txtUsuarioProfesor, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 300, 400, 20));
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel2.setText("Profesor");
         rSPanelGradiente1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 210, 150, -1));
-        rSPanelGradiente1.add(pswContrasenaProfesor, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 380, 400, -1));
+
+        pswContrasenaProfesor.setBorder(null);
+        rSPanelGradiente1.add(pswContrasenaProfesor, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 380, 400, 20));
 
         rSMaterialButtonRound1.setText("Log in");
         rSMaterialButtonRound1.addActionListener(new java.awt.event.ActionListener() {
@@ -201,35 +205,6 @@ public class frmLoginProfesor extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(frmLoginProfesor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(frmLoginProfesor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(frmLoginProfesor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(frmLoginProfesor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
